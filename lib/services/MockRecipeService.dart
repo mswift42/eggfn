385f5e5d5464ca8955f88edacfb2b8668c0fdd76
mswift42 <martin.haesler@gmail.com>
2017-04-06 14:03:28 +0100

    use local images for recipe thumbnails.

--- lib/services/MockRecipeService.dart
+++ lib/services/MockRecipeService.dart
@@ -6,7 +6,7 @@ final List<Recipe> mockrecipes = <Recipe>[
       publisher: "The Pioneer Woman",
       title: "French Toast",
       source_url: "http://www.simplyrecipes.com/recipes/french_toast/",
-      image_url: "http://static.food2fork.com/frenchtoast300x200b2da65d4.jpg",
+      image_url: "mockimages/frenchtoast300200.jpg",
       publisher_url: "http://www.simplyrecipes.com",
       recipe_id: "36355",
       ingredients: [
@@ -25,7 +25,7 @@ final List<Recipe> mockrecipes = <Recipe>[
       title: "French Onion Soup Stuffed Mushrooms",
       source_url:
           "http://thepioneerwoman.com/cooking/2009/11/french-onion-soup-stuffed-mushrooms/",
-      image_url: "http://static.food2fork.com/5200405868_e86da8e6e8_oceea.jpg",
+      image_url: "mockimages/frenchonionstuffedmushrooms.jpg",
       publisher_url: "http://thepioneerwoman.com",
       recipe_id: "47078",
       ingredients: [
@@ -44,7 +44,7 @@ final List<Recipe> mockrecipes = <Recipe>[
       title: "Easy Slow Cooker French Dip",
       source_url:
           "http://allrecipes.com/Recipe/Easy-Slow-Cooker-French-Dip/Detail.aspx",
-      image_url: "http://static.food2fork.com/103167cea.jpg",
+      image_url: "easyslowcookerfrenchdip.jpg",
       publisher_url: "http://allrecipes.com",
       recipe_id: "12360",
       ingredients: [
@@ -61,7 +61,7 @@ final List<Recipe> mockrecipes = <Recipe>[
       source_url:
           "http://www.closetcooking.com/2009/11/pumpkin-pie-french-toast.html",
       image_url:
-          "http://static.food2fork.com/PumpkinPieFrenchToast15002845c7fe.jpg",
+          "mockimages/pumpkinpiefrenchtoast.jpg",
       publisher_url: "http://closetcooking.com",
       recipe_id: "35499",
       ingredients: [
@@ -81,7 +81,7 @@ final List<Recipe> mockrecipes = <Recipe>[
       title: "Perfect French Fries",
       source_url:
           "http://thepioneerwoman.com/cooking/2012/01/perfect-french-fries/",
-      image_url: "http://static.food2fork.com/fries4d41.jpg",
+      image_url: "mockimages/frenchfries.jpg",
       publisher_url: "http://thepioneerwoman.com",
       recipe_id: "46959",
       ingredients: [
@@ -94,7 +94,7 @@ final List<Recipe> mockrecipes = <Recipe>[
       title: "Baked French Toast",
       source_url:
           "http://thepioneerwoman.com/cooking/2010/05/cinnamon-baked-french-toast/",
-      image_url: "http://static.food2fork.com/4573173616_0ed83c1ee1_o91e9.jpg",
+      image_url: "mockimages/backed-french-toast.jpg",
       publisher_url: "http://thepioneerwoman.com",
       recipe_id: "47139",
       ingredients: [
@@ -120,7 +120,7 @@ final List<Recipe> mockrecipes = <Recipe>[
       source_url:
           "http://www.twopeasandtheirpod.com/cannoli-stuffed-french-toast/",
       image_url:
-          "http://static.food2fork.com/cannolistuffedfrenchtoast48a37.jpg",
+          "mockimages/cannolistuffedfrenchtoast48a37.jpg",
       publisher_url: "http://www.twopeasandtheirpod.com",
       recipe_id: "54279",
       ingredients: [
@@ -139,7 +139,7 @@ final List<Recipe> mockrecipes = <Recipe>[
       title: "Overnight Blueberry French Toast",
       source_url:
           "http://allrecipes.com/Recipe/Overnight-Blueberry-French-Toast/Detail.aspx",
-      image_url: "http://static.food2fork.com/38151724f8.jpg",
+      image_url: "mockimages/overnightblueberryfrenchtoast.jpg",
       publisher_url: "http://allrecipes.com",
       recipe_id: "18647",
       ingredients: [
