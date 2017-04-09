0b507872331869178d655008af053eb1fed8e9c9
mswift42 <martin.haesler@gmail.com>
2017-04-09 12:47:41 +0100

    Add methods add/delete/isFavourite.

--- lib/services/FavouriteService.dart
+++ lib/services/FavouriteService.dart
@@ -5,6 +5,18 @@ class FavouriteService {
 
   Set<String> get favourites => _favourites;
 
+  void addFavourite(String recipeid) {
+    _favourites.add(recipeid);
+  }
+
+  void deleteFavourite(String recipeid) {
+    _favourites.remove(recipeid);
+  }
+
+  bool isFavourite(String recipeid) {
+    return _favourites.contains(recipeid);
+  }
+
 
 
 }
