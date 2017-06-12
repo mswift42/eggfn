22a2f0bdedc7e2676a388b6816a819bd98def9c6
mswift42 <martin.haesler@gmail.com>
2017-06-12 14:08:29 +0100

    Add test for EggCrackin Widget.

---
+++ test/recipeswidget_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:eggfn/ui/RecipesWidget.dart';
+
+void main() {
+  testWidgets('EggCrackin uses _kThemeData', (WidgetTester tester) async {
+    await tester.pumpWidget(new EggCrackin());
+    MaterialApp map = tester.widget(find.byType(MaterialApp));
+      expect(map.theme.brightness, Brightness.light);
+      expect(map.theme.accentColor, Colors.pink);
+      expect(map.theme.buttonColor, Colors.grey[200]);
+  });
+}
