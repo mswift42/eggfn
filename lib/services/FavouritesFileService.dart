8b3c63a699c46bab516893b2f50706e5069369af
mswift42 <martin.haesler@gmail.com>
2017-07-30 10:02:57 +0100

    Add file FavouriteFileService.

---
+++ lib/services/FavouritesFileService.dart
@@ -0,0 +1,37 @@
+import 'dart:async' show Future;
+import 'dart:io' show File;
+import 'dart:convert' show JSON;
+import 'package:path_provider/path_provider.dart'
+    show getApplicationDocumentsDirectory;
+import 'package:eggfn/services/Recipe.dart' show Recipe;
+
+class FavouritesFileService {
+  static Future<File> _getLocalFile() async {
+    String dir = (await getApplicationDocumentsDirectory()).path;
+    return new File('$dir/favourites.json');
+  }
+
+  static String _convertFavouritesToJson(List<Recipe> favourites) {
+    List favmap = favourites.map((i) => i.toJson()).toList();
+    return JSON.encode(favmap);
+  }
+
+  static Future<Null> saveFavourites(List<Recipe> favourites) async {
+    String contents = _convertFavouritesToJson(favourites);
+    await (await _getLocalFile()).writeAsString(contents);
+  }
+
+  static List<Recipe> _convertToRecipes(String contents) {
+    List parsedList = JSON.decode(contents);
+    return parsedList
+            .map((i) => new Recipe(
+                publisher: i["publisher"],
+                title: i["title"],
+                sourceUrl: i["source_url"],
+                imageUrl: i["image_url"],
+                publisherUrl: i["publisher_url"],
+                recipeID: i["recipe_id"]))
+            .toList() ??
+        new List();
+  }
+}
