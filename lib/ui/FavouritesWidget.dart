af6b1215b2e1594aab3d891e99de9192ada17441
mswift42 <martin.haesler@gmail.com>
2017-08-03 15:15:13 +0100

    Add favouriteWidget skeleton.

--- lib/ui/FavouritesWidget.dart
+++ lib/ui/FavouritesWidget.dart
@@ -1,1 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:eggfn/services/Recipe.dart';
+import 'package:eggfn/services/FavouritesFileService.dart';
+import 'dart:async';
+
+class FavouritesWidget extends StatefulWidget {
+
+  _FavouritesState createState() => new _FavouritesState();
+}
+
+class _FavouritesState extends State<FavouritesWidget> {
+ List<Recipe> _favourites = new List<Recipe>();
+
+  @override
+  Widget build(BuildContext context) {
+    return new GridView.extent(
+      children: _favourites.map((i) => new FavouriteWidget(
+        recipe: i,
+      )).toList(),
+        maxCrossAxisExtent: 400.00);
+  }
+}
+
+class FavouriteWidget extends StatelessWidget {
+  final Recipe recipe;
+  FavouriteWidget({this.recipe});
+
+  @override
+  Widget build(BuildContext context) {
+    return new GridTile(
+        child: null);
+  }
+}
