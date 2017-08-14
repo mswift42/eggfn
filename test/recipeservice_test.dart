b5f92a6bc846a463c9bc60bff852b3f03ff718c3
mswift42 <martin.haesler@gmail.com>
2017-08-14 11:13:21 +0100

    Add check for queryUrl method.

---
+++ test/recipeservice_test.dart
@@ -0,0 +1,15 @@
+import 'package:eggfn/services/RecipeService.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  var key = '7987c43afcf8a03a964bbcb0c9152c84';
+  test('expect queryUrl to return correct Url', () {
+    RecipeService rs = new RecipeService();
+    var qu = rs.queryUrl("bacon");
+    expect(qu, "http://food2fork.com/api/search?key=$key&q=bacon");
+    var qu1 = rs.queryUrl("bacon,eggs");
+    expect(qu1, "http://food2fork.com/api/search?key=$key&q=bacon,eggs");
+    var qu2 = rs.queryUrl("bacon,eggs tomatoes");
+    expect(qu2, "http://food2fork.com/api/search?key=$key&q=bacon,eggs+tomatoes");
+  });
+}
