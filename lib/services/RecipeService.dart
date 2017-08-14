54bd08ebe9fdce49ea5c4baeded8135b2cb14110
mswift42 <martin.haesler@gmail.com>
2017-08-14 10:01:58 +0100

    Add static method queryUrl.i

--- lib/services/RecipeService.dart
+++ lib/services/RecipeService.dart
@@ -1,12 +1,17 @@
 import 'package:eggfn/services/Recipe.dart' show Recipe;
 
 class RecipeService {
-  final String _food2forkapikey = '7987c43afcf8a03a964bbcb0c9152c84';
+  static final String food2forkapikey = '7987c43afcf8a03a964bbcb0c9152c84';
 
-  String get food2forkapikey => _food2forkapikey;
 
   static String queryUrl(String query) {
     var split = query.trim().split(" ");
-    var url = ""
+    var key = food2forkapikey;
+    var url = "http://food2fork.com/api/search?key=$key&q=";
+    url += split[0];
+    for (var s in split.sublist(1)) {
+      url += "+" + s;
+    }
+    return url;
   }
 }
