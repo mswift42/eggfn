decbca8045af9f13b5720a67fbc8afd0da86a7f9
mswift42 <martin.haesler@gmail.com>
2017-08-14 11:18:11 +0100

    Add test for queryPage method.

--- test/recipeservice_test.dart
+++ test/recipeservice_test.dart
@@ -12,4 +12,12 @@ void main() {
     var qu2 = rs.queryUrl("bacon,eggs tomatoes");
     expect(qu2, "http://food2fork.com/api/search?key=$key&q=bacon,eggs+tomatoes");
   });
+  test('expect queryPage to add correct pagenumber', () {
+     RecipeService rs = new RecipeService();
+     var queryurl = "http://food2fork.com/api/search?key=$key&q=bacon,eggs";
+     var qp = rs.queryPage(queryurl, 2);
+     expect(qp, queryurl + "&page=2");
+    var qp2 = rs.queryPage(queryurl, 14);
+    expect(qp2, "http://food2fork.com/api/search?key=$key&q=bacon,eggs&page=14");
+  });
 }
