f81ddb83b9a5638cdb44c786a5f968eba17d7fa7
mswift42 <martin.haesler@gmail.com>
2017-08-14 11:08:22 +0100

    Don't use purely static methods.

--- lib/services/RecipeService.dart
+++ lib/services/RecipeService.dart
@@ -1,17 +1,22 @@
 import 'package:eggfn/services/Recipe.dart' show Recipe;
 
 class RecipeService {
-  static final String food2forkapikey = '7987c43afcf8a03a964bbcb0c9152c84';
+   final String _food2forkapikey = '7987c43afcf8a03a964bbcb0c9152c84';
 
 
-  static String queryUrl(String query) {
+
+   String queryUrl(String query) {
     var split = query.trim().split(" ");
-    var key = food2forkapikey;
+    var key = _food2forkapikey;
     var url = "http://food2fork.com/api/search?key=$key&q=";
     url += split[0];
     for (var s in split.sublist(1)) {
       url += "+" + s;
     }
     return url;
+  }
+
+  String queryPage(String query, int pagenumber) {
+    return '$query&page=$pagenumber';
   }
 }
