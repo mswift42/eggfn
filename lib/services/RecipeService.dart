8e29a73af5f78649dde613fc2e9efe0ae90899df
mswift42 <martin.haesler@gmail.com>
2017-08-14 09:56:51 +0100

    Add apikey.

--- lib/services/RecipeService.dart
+++ lib/services/RecipeService.dart
@@ -1,2 +1,12 @@
 import 'package:eggfn/services/Recipe.dart' show Recipe;
 
+class RecipeService {
+  final String _food2forkapikey = '7987c43afcf8a03a964bbcb0c9152c84';
+
+  String get food2forkapikey => _food2forkapikey;
+
+  static String queryUrl(String query) {
+    var split = query.trim().split(" ");
+    var url = ""
+  }
+}
