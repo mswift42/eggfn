8aeb21468c55b4db1a3a576222446193e9bb9e58
mswift42 <martin.haesler@gmail.com>
2017-08-19 10:40:38 +0100

    Add method getIngredients.

--- lib/services/RecipeService.dart
+++ lib/services/RecipeService.dart
@@ -1,4 +1,7 @@
 import 'package:eggfn/services/Recipe.dart' show Recipe;
+import 'dart:async';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
 class RecipeService {
    final String _food2forkapikey = '7987c43afcf8a03a964bbcb0c9152c84';
@@ -14,6 +17,22 @@ class RecipeService {
       url += "+" + s;
     }
     return url;
+  }
+
+  Future<String> getRecipes(String query) async {
+    var url = queryUrl(query);
+    var httpClient = createHttpClient();
+    var response = await httpClient.read(url);
+    print(response);
+    return response;
+  }
+
+  Future<List<String>> getIngredients(String recipeid) async {
+      var url = "http://food2fork.com/api/get?key=$_food2forkapikey&rId=$recipeid";
+      var httpClient = createHttpClient();
+      var response = await httpClient.read(url);
+      var decoded = JSON.decode(response)["recipe"]["ingredients"];
+      return decoded;
   }
 
   String queryPage(String query, int pagenumber) {
